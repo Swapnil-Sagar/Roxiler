cf3c6c3505d78c99a04b72490099d1f08f442bb2
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2021-03-13 03:40:17 +0530

    added table and search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,124 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
 
 function App() {
-  return <div className="App">Roxilier Assignment</div>;
+  const classes = useStyles();
+  const [task, setTask] = useState([]);
+  const [search, setSearch] = useState("");
+  const getTodoData = async () => {
+    try {
+      const data = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      console.log(data.data);
+      setTask(data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getTodoData();
+  }, []);
+  return (
+    <div className="App">
+      <h1>Roxiler</h1>
+      <input
+        type="text"
+        placeholder="Search Here.."
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {/* {task
+        .filter((item) => {
+          if (search == "") {
+            return item;
+          } else if (item.title.toLowerCase().includes(search.toLowerCase())) {
+            return item;
+          }
+        })
+        .map((item) => {
+          return (
+            <p>
+              {item.id} - {item.title} - {item.completed}
+            </p>
+          );
+        })} */}
+
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>ToDo ID</StyledTableCell>
+              <StyledTableCell align="left">Title</StyledTableCell>
+              <StyledTableCell align="right">Status</StyledTableCell>
+              <StyledTableCell align="right">Action</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {task
+              .filter((item) => {
+                if (search == "") {
+                  return item;
+                } else if (
+                  item.title.toLowerCase().includes(search.toLowerCase())
+                ) {
+                  return item;
+                }
+              })
+              .map((item) => {
+                return (
+                  <StyledTableRow key={item.id}>
+                    <StyledTableCell component="th" scope="row">
+                      {item.id}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">{item.title}</StyledTableCell>
+                    <StyledTableCell align="left">
+                      {item.completed}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">
+                      {item.action}
+                    </StyledTableCell>
+                  </StyledTableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
 
 export default App;
